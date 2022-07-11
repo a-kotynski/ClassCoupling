e5b2b7e5d6743dc9abad4a89ea4bd3762a5a63b5
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-11 08:50:35 +0200

    Add project files.

---
+++ ClassCoupling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2243552#content
+
+// Coupling is a measure of how interconnected classes and subsystems are
+// my explanation: A way to describe interconnections of classes
+
+// A good practice is to create a loosely coupled application
+
+// Types of Relationships:
+//  * Inheritance
+//  * Composition
+
+namespace ClassCoupling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
